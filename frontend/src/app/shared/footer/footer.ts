82a421c27661ad497981430507b4b1259ae06c92
Mahmoud Tarek <mahmod122tarek@gmail.com>
2026-07-02 01:37:08 +0300

    fixid footer andf header in layout

--- frontend/src/app/shared/footer/footer.ts
+++ frontend/src/app/shared/footer/footer.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HomeService } from '../../core/service/home.service';
+import { IHome } from '../../core/model/home.model';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +9,25 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer implements OnInit {
+  constructor(private __homeService: HomeService) {}
+
+  footerData!: IHome;
+  firstChar: string = '';
+  lastName: string = '';
+
+  ngOnInit(): void {
+    this.__homeService.getData().subscribe((value) => {
+      this.footerData = value;
+      const arrName = this.footerData.name.split(' ');
+      if (arrName.length > 0) {
+        this.firstChar = arrName[0];
+        if (arrName.length > 1) {
+          this.lastName = arrName[arrName.length - 1];
+        } else {
+          this.lastName = '';
+        }
+      }
+    });
+  }
+}
